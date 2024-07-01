68c800977da8bc2a229ce36e1e6bcf2d87a83b28
Juan Plasencia <jplasencia.dev@outlook.com>
2024-07-01 17:20:34 -0500

    format and readme update

--- README.md
+++ README.md
@@ -35,3 +35,34 @@ npm start
 ```
 
 The application will run on [http://localhost:8080](http://localhost:8080) by default.
+
+
+## API Endpoints
+
+Here are the available API endpoints:
+
+- **GET** `/leads` - Retrieve all leads.
+- **GET** `/leads/:id` - Retrieve a lead by ID.
+- **POST** `/leads` - Register a new lead.
+- **PUT** `/leads/:id` - Modify a lead by ID.
+- **DELETE** `/leads/:id` - Delete a lead by ID.
+
+
+## Documentation
+
+You can find the API documentation at the following URL:
+[https://leads-directory.onrender.com/api-docs](https://leads-directory.onrender.com/api-docs)
+
+### API documented example uses (.rest)
+
+Also, it is possible to find .rest files to test the different end points with documented examples, in this path `/docs/api`:
+
+- **development** `/docs/api/development.rest` - with local host and development environment.
+- **production** `/docs/api/production.rest` - with render.com and production environment.
+
+### Swagger Setup
+
+We use Swagger for API documentation. To view the Swagger UI locally, follow these steps:
+
+1. Ensure your application is running.
+2. Open your browser and navigate to [http://localhost:8080/api-docs](http://localhost:8080/api-docs).
--- src/models/leadModel.js
+++ src/models/leadModel.js
@@ -1,39 +1,34 @@
 const mongodb = require("../database");
 
 async function getAllLeads() {
-    try {
-        const result = await mongodb
-            .getDb()
-            .db()
-            .collection("lead")
-            .find({})
-            .toArray();
-        console.log("executed query: " + getAllLeads.name)
-        return result;
-    } catch (error) {
-        console.log(
-          "No contact collection found error: " +
-            getAllLeads.name +
-            ", " +
-            error,
-        );
-    }
+  try {
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("lead")
+      .find({})
+      .toArray();
+    console.log("executed query: " + getAllLeads.name);
+    return result;
+  } catch (error) {
+    console.log(
+      "No contact collection found error: " + getAllLeads.name + ", " + error,
+    );
+  }
 }
 
 async function saveLead(newLead) {
-    try {
-        const result = await mongodb
-          .getDb()
-          .db()
-          .collection("lead")
-          .insertOne(newLead);
-        console.log("executed query: " + saveLead.name);
-        return result;
-    } catch (error) {
-        console.error(
-          "Lead was not saved error " + saveLead.name + ", " + error
-        );
-    }
+  try {
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("lead")
+      .insertOne(newLead);
+    console.log("executed query: " + saveLead.name);
+    return result;
+  } catch (error) {
+    console.error("Lead was not saved error " + saveLead.name + ", " + error);
+  }
 }
 
 module.exports = {
